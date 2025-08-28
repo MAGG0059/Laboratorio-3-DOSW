cb471c0af813d8d6467e891d675c394cf5d86a25
MAGG0059 <manuel.guarnizo-g@mail.escuelaing.edu.co>
2025-08-28 18:34:56 -0500

    feature:; parte 1 6

---
+++ laboratorio3dosw/src/main/java/edu/dosw/lab/laboratorio3dosw/Laboratorio3doswApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.laboratorio3dosw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio3doswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio3doswApplication.class, args);
+	}
+
+}
---
+++ laboratorio3dosw/src/test/java/edu/dosw/lab/laboratorio3dosw/Laboratorio3doswApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.laboratorio3dosw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio3doswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
