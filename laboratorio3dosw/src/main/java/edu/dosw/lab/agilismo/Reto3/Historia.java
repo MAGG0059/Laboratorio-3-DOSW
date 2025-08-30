725e43ef2ee2547cd8a53c3cdcf823ba886c3ce4
daniel.rodriguez-b <daniel.rodriguez-b@mail.escuelaing.edu.co>
2025-08-29 19:06:23 -0500

    Parte 2 - Reto 3 Daniel Alejandro Rodriguez: Clase de historia

---
+++ laboratorio3dosw/src/main/java/edu/dosw/lab/agilismo/Reto3/Historia.java
@@ -0,0 +1,22 @@
+package edu.dosw.lab.agilismo.Reto3;
+
+class Historia {
+    private String nombre;
+    private int puntajeFinal = -1;
+
+    public Historia(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntajeFinal() {
+        return puntajeFinal;
+    }
+
+    public void setPuntajeFinal(int puntaje) {
+        this.puntajeFinal = puntaje;
+    }
+}
